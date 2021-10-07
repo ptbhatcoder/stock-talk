20972b995b6b0d605c0a557d5ec861d5dc840fdf
Pranav Thirthahalli Bhat <pranbhat@microsoft.com>
2021-10-08 01:50:56 +0530

    Adding the search and submit button

--- src/stock-search/components/StockSearchScreen.tsx
+++ src/stock-search/components/StockSearchScreen.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useState } from "react";
 import { SearchResult, useStockAPIManager } from "../../api-manager";
 import { useDebounce } from "../../common/hooks/useDebounce";
+import { useOpenDetails } from "../../common/hooks/useOpenDetails";
 import { StockSearchResults } from "./StockSearchResults";
 import "./StockSearchScreen.css";
 
@@ -18,21 +19,48 @@ export const StockSearchScreen: React.FC<{}> = () => {
   const stockAPIManager = useStockAPIManager();
   useDebounce(
     () => {
-      stockAPIManager?.fetchSearchResults(query)?.then(setSearchResults);
+      stockAPIManager?.fetchSearchResults(query)?.then((results) => {
+        setSearchResults(
+          [...results].sort((r1, r2) => +r2.matchScore - +r1.matchScore)
+        );
+      });
     },
     300,
     [query, stockAPIManager]
   );
 
+  const openDetails = useOpenDetails();
+
+  const onSubmitClick = () => {
+    const timeOut = setTimeout(() => {
+      clearTimeout(timeOut);
+      if (searchResults.length) {
+        openDetails(searchResults[0].symbol);
+      } else {
+        alert(`Invalid symbol ${query}`);
+      }
+    }, 0);
+  };
+
   return (
     <div className="searchScreen">
-      <input
-        type="text"
-        className="searchBar"
-        placeholder="Search for a stock here"
-        value={query}
-        onChange={onQueryChange}
-      />
+      <h1 style={{ textAlign: "center" }}>Welcome to Stock Talk</h1>
+      <div className="searchBarAndButton">
+        <input
+          type="text"
+          className="searchBar"
+          placeholder="Search for a stock here"
+          value={query}
+          onChange={onQueryChange}
+        />
+        <button type="submit" className="searchButton" onClick={onSubmitClick}>
+          <img
+            src="https://www.pngfind.com/pngs/m/59-593293_search-zoom-magnifier-magnifying-glass-comments-search-icon.png"
+            alt=""
+          />
+        </button>
+      </div>
+
       <StockSearchResults results={searchResults} />
     </div>
   );
